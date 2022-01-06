1a457de70ada41dbdfbc46d3f34b53ec8418cbb9
gannon145 <matthewtwayne@gmail.com>
2022-01-05 20:24:03 -0500

    Intercept V1
    Added passive Red intercept targets spawnable via F10
    added A-4 spawns

--- PROJECT_Intercept.lua
+++ PROJECT_Intercept.lua
@@ -6,120 +6,58 @@ env.info("Intercept Loading", false)
 InterceptName = "RedIntercept"
 
 InterceptGrp = { 
-          --"VVS Tu-95MS",
+          "VVS Tu-95MS",
           "VVS Tu-160",
           "VVS Tu-22M3",
           "SyAAF An-26B",
-          --"IRIAF F-14A",
-          --"IRIAF F-4E",
+          "IRIAF F-14A",
+          "IRIAF F-4E",
           } 
    
 InterceptZone = ZONE:FindByName("Intercept") 
               
 ---------------------------------------------------------------------------------------------------
---- Spawn Function
+--- Spawn/Delete Functions
 ---------------------------------------------------------------------------------------------------
-
 function SEF_SPAWNINTERCEPT ()
-  Bandit = SPAWN:NewWithAlias("InterceptPlane", InterceptName):InitRandomizeTemplate(InterceptGrp)
+  Bandit = SPAWN:NewWithAlias("InterceptPlane", InterceptName)
+    :InitRandomizeTemplate(InterceptGrp)
     :OnSpawnGroup(
     
-    function (groupSpawned)
-        --local playergrp = GROUP:GetPlayerUnits() --GROUP:FindByName( "Viper" )
-        bandit_zone = ZONE_GROUP:New( "SWITCH_ZONE", groupSpawned, 20000 )
+      function (groupSpawned)
+        bandit_zone = ZONE_GROUP:New( "SWITCH_ZONE", groupSpawned, 1610 )
         bandit_in_zone = SCHEDULER:New( nil,
+
             function()
-               if ZONE:FindByName("SWITCH_ZONE"):CheckScannedCoalition(coalition.side.BLUE) == true
-               then
-                  MESSAGE:New('Bandit sucessfully intercepted', 30, 'Info'):ToAll()
-                  Bandit:RouteRTB()
+              bandit_zone:Scan( {Object.Category.UNIT}, {Unit.Category.AIRPLANE} ) -- SCAN: Look for airplanes in Zone
+              --BASE:E(bandit_zone.ScanData) -- DEBUG: LOG SCAN DATA
+              --SCHEDULER:New(nil,(bandit_zone:DrawZone(-1, {1,0,0}, 1, {1,0,0}, 0.15, 1)), 5, 15) -- DEBUG: SEE MOVING ZONE
+              if bandit_zone:CheckScannedCoalition(coalition.side.BLUE) == true then -- SCAN: If Blue in zone, RTB Bandit
+                MESSAGE:New('Bandit sucessfully intercepted', 30, 'Info'):ToAll()
+                Patrol:RTB()
               end
-            end, 
-         {}, 2, 5)
-     end
-     )
-   :Spawn()
+            end
+          , {}, 0, 10)
+          
+      end
+      
+    )
+   :SpawnInZone(InterceptZone, true, 2000, 12000)
+   --Patrol = AI_A2A_CAP:New(Bandit, InterceptZone, 3000, 12000, 400, 600, 600, 1600) -- For Agreesive CAP when implemented
+   --Patrol:SetHomeAirbase(AIRBASE.Syria.Gazipasa)-- For Agreesive CAP when implemented
+   Patrol = AI_PATROL_ZONE:New( InterceptZone, 3000, 12000, 400, 600 )
+   Patrol:ManageFuel( 0.1, 60 )
+   Patrol:SetControllable( Bandit )
+   Patrol:__Start( 20 )
 end
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---[[
-function SEF_PatrolRTB ()
-  GroupName = GROUP:FindByName("RedIntercept")
+function SEF_PatrolDEL ()
+  GroupName = GROUP:FindByName("RedIntercept#001")
+  Patrol:Stop()
   GroupName:Destroy(true)-- Send group above to RTB
-  MESSAGE:New("You have completed the intercept!", 15):ToAll()--and send message to coallition 
+  MESSAGE:New("Intercept has been removed", 15):ToAll()--and send message to coallition 
 end
-
-function SEF_SPAWNINTERCEPT ()
-
-  PatrolSpawn = SPAWN:NewWithAlias("InterceptPlane", InterceptName):InitRandomizeTemplate(InterceptGrp)--:SpawnInZone(InterceptZone, true, 3000, 12000)
-  PatrolGroup = PatrolSpawn:Spawn()
-  Patrol = AI_PATROL_ZONE:New( InterceptZone, 3000, 12000, 400, 600 )
-  Patrol:ManageFuel( 0.1, 60 )
-  Patrol:SetControllable( PatrolGroup )
-  Patrol:__Start( 5 )
-  
-  Threat = GROUP:FindByName(InterceptName)
-  InterceptZone = ZONE_GROUP:New("RTBzone", Threat, 50000) -- Create zone around intercept target
-  GroupObject = GROUP:GetPlayerUnits()  -- Get list of players
-  if
-    --Zone:E( { "Group is completely in Zone:", GroupObject:IsPartlyInZone( InterceptZone ) } ) -- is player within 3000m of target to force RTB?
-  then
-    SEF_PatrolRTB ()-- Send group above to RTB 
-    --MESSAGE:New("You have intercepted the threat and they are RTB!", 15):ToAll()--and send message to coallition
-  end
-   
-end 
-]]--
-
-
-
----------------------------------------------------------------------------------------------------
---- Delete After Land Function
----------------------------------------------------------------------------------------------------
-
---[[
-DeleteLanding = EVENTHANDLER:New()
-DeleteLanding:HandleEvent( EVENTS.Land )
-function DeleteLanding:OnEventLand( EventData )
-ThisGroup = GROUP:FindByName(EventData.IniGroupName)
-GroupUnit = ThisGroup:GetDCSUnit(1)
-FirstUnit = UNIT:Find(GroupUnit)
-  if FirstUnit:GetPlayerName() then
-    PlayerName = FirstUnit:GetPlayerName()
-    env.info(PlayerName .. " has landed")
-  else 
-    env.info("Not a player landed, deleting")
-    ScheduleDelete(ThisGroup)-- custom schedule to delete a group
-  end
-end
-
-function ScheduleDelete(group)
-  SCHEDULER:New( nil, function() 
-    env.info("Fleet: Destroying landed group")
-    if group then
-    group:Destroy()
-    end
-    end, {}, 90, 9999, 1, 62)
-end
-]]--
-
 
 ---------------------------------------------------------------------------------------------------
 --- Menu
@@ -129,7 +67,7 @@ InterceptMenuTop = MENU_COALITION:New(coalition.side.BLUE, "INTERCEPT SPAWNS")
 function addInterceptMenu()
 
   MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Spawn Intercept Group", InterceptMenuTop, SEF_SPAWNINTERCEPT)
-  MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Intercept RTB", InterceptMenuTop, SEF_PatrolRTB)
+  MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Delete Intercept Unit", InterceptMenuTop, SEF_PatrolDEL)
   
 end
 
